8133d6fdcdf9606d019b1eed7fd7d08d25b3d830
nebula1989 <benwalterscoding@gmail.com>
2020-09-06 12:04:58 -0400

    make script scan and report a batch of urls, 4 at at time, then rest for 1 minute.

--- vt_scan_report.py
+++ vt_scan_report.py
@@ -14,25 +14,65 @@ vtotal = Virustotal(api_key)  # virustotal API key
 
 
 def main():
-    print("Welcome.  Input 1 or more urls: ")
-    url = input()
-    vt_scan(url)
-    vt_report(url)
+    print("Welcome. Enter 1 or more URLS:")
+    urls = get_urls()
+    vt_scan_report(urls)
 
 
-def vt_scan(url):
-    result = vtotal.url_scan(
-        [url]
-    )
-    pprint(result)
+def get_urls():
+    lines = []
+    while True:
+        line = input()
+        if line:
+            lines.append(line)
+        else:
+            break
+    return lines
 
 
-def vt_report(url):
-    result = vtotal.url_report(
-        [url]
-    )
+def vt_scan_report(urls_list):
+    # THINK OF A WAY TO SCAN ONLY 4 URLS PER MINUTE IN A LIST OF X URLS.
 
-    pprint(result)
+    already_scanned_urls = []
+
+    scan_limit_four_at_time = 0
+
+    for i in range(0, len(urls_list)):
+        if urls_list[i] not in already_scanned_urls:
+            print("\nScanning: " + urls_list[i])
+            vtotal.url_scan(
+                [urls_list[i]]
+            )
+
+            countdown(3)
+            print("\nPrinting Report:\n")
+            result = vtotal.url_report(
+                [urls_list[i]]
+            )
+
+            already_scanned_urls.append(urls_list[i])
+            scan_limit_four_at_time += 1
+
+            print("\n***********")
+            pprint(result)
+            print("*************\n")
+
+            print("Already Scanned: " + str(already_scanned_urls))
+            print("Scan Limit: " + str(scan_limit_four_at_time))
+
+        if scan_limit_four_at_time > 0 and scan_limit_four_at_time % 4 == 0:
+            print("Taking 1 minute break... using free VirusTotal Api, which limits to 4 per minute..")
+            countdown(61)
+            continue
+
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
 
 
 if __name__ == '__main__':
