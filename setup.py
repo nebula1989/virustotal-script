f01850ee715d4e5302afe80eb19950748d26e725
nebula1989 <benwalterscoding@gmail.com>
2020-09-13 19:03:24 -0400

    created setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+PACKAGE_DIR = r'C:\Users\benwa\ProgrammingProjects\VirusTotalStuff\env\Lib\site-packages'
+
+setup(
+    name='VirusTotalStuff',
+    version='1.0',
+    packages=[PACKAGE_DIR],
+    url='',
+    license='',
+    author='Benjamin Walters',
+    author_email='',
+    description='python script.  uses virustotal api to scan batch of URLs'
+)
