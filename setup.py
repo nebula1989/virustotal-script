a8799ff7b18a1ce47f370d0bc677d905e41407c8
nebula1989 <benwalterscoding@gmail.com>
2020-09-14 12:08:04 -0400

    create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='VirusTotalStuff',
+    version='0.01',
+    packages=find_packages(),
+    url='https://github.com/nebula1989/virustotal-script',
+    license='MIT',
+    author='Benjamin Walters',
+    author_email='benwalterscoding@gmail.com',
+    description='script to scan many urls using  virustotal api'
+)
