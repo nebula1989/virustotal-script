740b1b392eab3851a2f747db3372cf360517ea2a
nebula1989 <benwalterscoding@gmail.com>
2020-09-14 15:47:54 -0400

    type in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='VirusTotalStuff',
-    version='0.01',
+    version='0.0.1',
     packages=find_packages(),
     url='https://github.com/nebula1989/virustotal-script',
     license='MIT',
